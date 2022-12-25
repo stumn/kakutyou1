7ee8b8dcc14ddcca65ea94f12ed8d5a64b331d36
stumn <pukut0317@gmail.com>
2022-12-25 13:07:23 +0900

    initial commit

---
+++ content_script_rakuten.js
@@ -0,0 +1,11 @@
+let kokoku= document.querySelectorAll("#pitari_sticky");
+for(let i = 0; i < kokoku.length; i++){
+  let e = kokoku[i];
+  e.style.display = 'none';
+}
+
+let jama = document.querySelectorAll(".floating-cart-wrapper");
+for(let i = 0; i < jama.length; i++){
+  let e = jama[i];
+  e.style.display = 'none';
+}
