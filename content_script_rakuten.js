fa8a4a806c283ab240e96b223adf7ad49910ac63
stumn <pukut0317@gmail.com>
2022-12-25 13:22:38 +0900

    rakuten change

--- content_script_rakuten.js
+++ content_script_rakuten.js
@@ -1,11 +1,23 @@
-let kokoku= document.querySelectorAll("#pitari_sticky");
-for(let i = 0; i < kokoku.length; i++){
-  let e = kokoku[i];
+let big_ad = document.querySelectorAll("#bigBannerWidget");
+for(let i = 0; i < big_ad.length; i++){
+  let e = big_ad[i];
   e.style.display = 'none';
 }
 
-let jama = document.querySelectorAll(".floating-cart-wrapper");
-for(let i = 0; i < jama.length; i++){
-  let e = jama[i];
+let pop_rank = document.querySelectorAll("#rnkAsurakuEventContentsHasHistory");
+for(let i = 0; i < pop_rank.length; i++){
+  let e = pop_rank[i];
+  e.style.display = 'none';
+}
+
+let mobile = document.querySelectorAll("#mno-header");
+for(let i = 0; i < mobile.length; i++){
+  let e = mobile[i];
+  e.style.display = 'none';
+}
+
+let left = document.querySelectorAll("#side-navi");
+for(let i = 0; i < left.length; i++){
+  let e = left[i];
   e.style.display = 'none';
 }
