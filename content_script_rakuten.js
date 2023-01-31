4927eb2d680a2b82116b08c0028f731dcc0c62b6
stumn <pukut0317@gmail.com>
2023-01-31 11:50:19 +0900

    pubric_info deletet

--- content_script_rakuten.js
+++ content_script_rakuten.js
@@ -58,8 +58,8 @@ for(let i = 0; i < pubric_info.length; i++){
   e.style.display = 'none';
 }
 
-let element = document.createElement("button"); // <button></button> ができる
-element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-let target = document.querySelector("#create_and_append"); // 追加したい要素を見つけてくる
-target.appendChild(element); // 追加する
+// let element = document.createElement("button"); // <button></button> ができる
+// element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
+// let target = document.querySelector("#create_and_append"); // 追加したい要素を見つけてくる
+// target.appendChild(element); // 追加する
 
