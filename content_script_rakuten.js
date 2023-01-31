5c712ebbb0a74baa60b30de350b87520ed1b2862
stumn <pukut0317@gmail.com>
2023-01-31 11:30:19 +0900

    button

--- content_script_rakuten.js
+++ content_script_rakuten.js
@@ -52,4 +52,8 @@ for(let i = 0; i < r_mag.length; i++){
   e.style.display = 'none';
 }
 
+let element = document.createElement("button"); // <button></button> ができる
+element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
+let target = document.querySelector("#create_and_append"); // 追加したい要素を見つけてくる
+target.appendChild(element); // 追加する
 
