be91ed163e959adbe0d96bd3c8f97e9acc00fb24
stumn <pukut0317@gmail.com>
2023-02-07 00:40:25 +0900

    change

--- content_script_rakuten.js
+++ content_script_rakuten.js
@@ -1,66 +1,82 @@
-let big_ad = document.querySelectorAll("#bigBannerWidget");
-for(let i = 0; i < big_ad.length; i++){
-  let e = big_ad[i];
-  e.style.display = 'none';
+addFeaturesAttempt();
+
+function addFeaturesAttempt(){
+  if(!addButtons()) setTimeout(addFeaturesAttempt, 500); // retry after 500 millisecconds
 }
 
-let pop_rank = document.querySelectorAll("#rnkAsurakuEventContentsHasHistory");
-for(let i = 0; i < pop_rank.length; i++){
-  let e = pop_rank[i];
-  e.style.display = 'none';
-}
-
-let mobile = document.querySelectorAll("#mno-header");
-for(let i = 0; i < mobile.length; i++){
-  let e = mobile[i];
-  e.style.display = 'none';
-}
-
-let left = document.querySelectorAll("#side-navi");
-for(let i = 0; i < left.length; i++){
-  let e = left[i];
-  e.style.display = 'none';
-}
-
-let pick_up = document.querySelectorAll("#sc_ad_pickup_normal");
-for(let i = 0; i < pick_up.length; i++){
-  let e = pick_up[i];
-  e.style.display = 'none';
-}
-
-let famous_shop = document.querySelectorAll("#sc_sidAdd_rBrand");
-for(let i = 0; i < famous_shop.length; i++){
-  let e =  famous_shop[i];
-  e.style.display = 'none';
-}
-
-let recommend = document.querySelectorAll(".ad-slideshow-existItemDisplay ri-main-contents");
-for(let i = 0; i < recommend.length; i++){
-  let e =  recommend[i];
-  e.style.display = 'none';
-}
-
-let time_sale = document.querySelectorAll("#sc_ad_timesale_normal");
-for(let i = 0; i < time_sale.length; i++){
-  let e =  time_sale[i];
-  e.style.display = 'none';
-}
-
-let r_mag = document.querySelectorAll("#sc_lidAdd_rmagazine"); //idだから＃を使う
-for(let i = 0; i < r_mag.length; i++){
-  let e =  r_mag[i];
-  e.style.display = 'none';
-}
-
-let pubric_r_info = document.querySelectorAll(".group-section"); //classだから.を使う
-for(let i = 0; i < pubric_r_info.length; i++){
-  let e = pubric_r_info[i];
-  e.style.display = 'none';
-  }
+function addButtons(){
+  const existing_button = document.querySelector(".main-buttons-container--2GZLJ"); //追加したい要素＝myクーポンのとなり
+  if(!existing_button) return false;
 
   let element = document.createElement("button"); // <button></button> ができる
   element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-  let target = document.querySelector(".main-buttons-container--2GZLJ"); // 追加したい要素を見つけてくる
-  target.appendChild(element); // 追加する
-  
-  
+  element.onclick = ButtonClicked_pushme();
+  existing_button.appendChild(element); // 追加する
+  return true;
+}
+
+function ButtonClicked_pushme(){
+  Delete();
+}
+
+function Delete(){
+  let big_ad = document.querySelectorAll("#bigBannerWidget");
+  for(let i = 0; i < big_ad.length; i++){
+    let e = big_ad[i];
+    e.style.display = 'none';
+  }
+
+  let pop_rank = document.querySelectorAll("#rnkAsurakuEventContentsHasHistory");
+  for(let i = 0; i < pop_rank.length; i++){
+    let e = pop_rank[i];
+    e.style.display = 'none';
+  }
+
+  let mobile = document.querySelectorAll("#mno-header");
+  for(let i = 0; i < mobile.length; i++){
+    let e = mobile[i];
+    e.style.display = 'none';
+  }
+
+  let left = document.querySelectorAll("#side-navi");
+  for(let i = 0; i < left.length; i++){
+    let e = left[i];
+    e.style.display = 'none';
+  }
+
+  let pick_up = document.querySelectorAll("#sc_ad_pickup_normal");
+  for(let i = 0; i < pick_up.length; i++){
+    let e = pick_up[i];
+    e.style.display = 'none';
+  }
+
+  let famous_shop = document.querySelectorAll("#sc_sidAdd_rBrand");
+  for(let i = 0; i < famous_shop.length; i++){
+    let e =  famous_shop[i];
+    e.style.display = 'none';
+  }
+
+  let recommend = document.querySelectorAll(".ad-slideshow-existItemDisplay ri-main-contents");
+  for(let i = 0; i < recommend.length; i++){
+    let e =  recommend[i];
+    e.style.display = 'none';
+  }
+
+  let time_sale = document.querySelectorAll("#sc_ad_timesale_normal");
+  for(let i = 0; i < time_sale.length; i++){
+    let e =  time_sale[i];
+    e.style.display = 'none';
+  }
+
+  let r_mag = document.querySelectorAll("#sc_lidAdd_rmagazine"); //idだから＃を使う
+  for(let i = 0; i < r_mag.length; i++){
+    let e =  r_mag[i];
+    e.style.display = 'none';
+  }
+
+  let pubric_r_info = document.querySelectorAll(".group-section"); //classだから.を使う
+  for(let i = 0; i < pubric_r_info.length; i++){
+    let e = pubric_r_info[i];
+    e.style.display = 'none';
+    }
+  }
